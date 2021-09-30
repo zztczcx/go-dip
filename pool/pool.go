6526dfec26f070ad3bc274951c43d37db79e77d7
chenxu <zztczcx@163.com>
2021-09-30 20:33:08 +1000

    finish rotating proxy, used a lot of channels

--- pool/pool.go
+++ pool/pool.go
@@ -1,12 +1,115 @@
 package pool
 
 import (
+  "errors"
+  "time"
+  "encoding/json"
+  "os"
+  "strings"
+  "fmt"
+  "log"
 )
 
+type Ip struct {
+  addr string
+  weight float32
+}
 
-func Run(){}
+type Proxy struct {
+  Ip string
+  Port string
+  Protocols []string
+  Latency float32
+}
 
-func Host() string{
-  return "127.0.0.1:8080"
+var ips = make([]Ip, 0)
 
+var queue = make(chan string)
+var push = make(chan Ip)
+var rotate = make(chan Ip)
+
+// fetch ips from external
+// test the ip's speed
+// maintain a pool
+func Run(){
+  go fetchIp()
+  go rotateIp()
+  go appendIp()
+
+  //waitGroup
+}
+
+func ChooseIp() (string, error){
+  if len(ips) == 0 {
+    return "", errors.New("ip pool is empty")
+  }else{
+    return <-queue, nil
+  }
+}
+
+var rotateDone = make(chan bool)
+
+func rotateIp() {
+  for {
+    if len(ips) == 0 {
+      time.Sleep(10*time.Second)
+
+    }else {
+      <- rotateDone
+      ip := ips[0]
+      fmt.Println("ip is ", ip)
+      queue <- ip.addr
+      rotate <- ip
+    }
+  }
+}
+ 
+func fetchIp() error{
+  fp, err := os.Open("./Free_Proxy_List.json")
+  defer fp.Close()
+
+  if err != nil {
+      log.Printf("open json file failed: %s", err.Error())
+      return err
+  }
+
+  var proxies []Proxy 
+
+  dec := json.NewDecoder(fp)
+  err = dec.Decode(&proxies)
+  if err != nil {
+      log.Printf("decode json file failed: %s", err.Error())
+      return err
+  }
+
+  for _, proxy := range proxies {
+    var sb strings.Builder
+    sb.WriteString(proxy.Ip)
+    sb.WriteString(":")
+    sb.WriteString(proxy.Port)
+
+    ip := Ip{addr: sb.String(), weight: proxy.Latency}
+    push <- ip
+  }
+
+  rotateDone <- true
+
+
+  for {
+    time.Sleep(time.Second * 300)
+    //check proxy speed
+  }
+}
+
+func appendIp(){
+  for {
+    select {
+
+      case ip := <- rotate:
+        ips = append(ips[1:], ip)
+        rotateDone <- true
+      case ip := <- push:
+        ips = append(ips, ip)
+    } 
+  }
 }
--- tunnel/tunnel.go
+++ tunnel/tunnel.go
@@ -8,29 +8,30 @@ import (
         "github.com/zztczcx/go-dip/pool"
 )
 
-func Forward(source *net.TCPConn, dest *net.TCPConn) {
-	defer dest.CloseWrite()
-	defer source.CloseRead()
-	io.Copy(dest, source)
-}
 func HandleConn(source *net.TCPConn) {
-	host := pool.Host()
-	if host == nil {
+	ip, err := pool.ChooseIp()
+	if err != nil {
 		source.Close()
-		log.Println("choose host failed")
+                log.Printf("choose ip failed: %s", err.Error())
 		return
 	}
 
-	dest, err := net.Dial("tcp", host)
+	dest, err := net.Dial("tcp", ip)
 	if err != nil {
 		source.Close()
-		log.Printf("connect to %s failed: %s", host, err.Error())
+		log.Printf("connect to %s failed: %s", ip, err.Error())
 		return
 	}
 
 	source.SetKeepAlive(true)
 	source.SetKeepAlivePeriod(time.Second * 60)
 
-	go Forward(source, dest.(*net.TCPConn))
-	go Forward(dest.(*net.TCPConn), source)
+	go forward(source, dest.(*net.TCPConn))
+	go forward(dest.(*net.TCPConn), source)
+}
+
+func forward(source *net.TCPConn, dest *net.TCPConn) {
+	defer dest.CloseWrite()
+	defer source.CloseRead()
+	io.Copy(dest, source)
 }
